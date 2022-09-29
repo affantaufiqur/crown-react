5bc2ca658b27cd01d4fbe44a74d19b5eadb51986
Affan <affantaufiqur@gmail.com>
2022-09-29 18:44:57 +0700

    feat: checkout page and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from './components/navigation/navigation.component'
 import Home from './routes/home/home.component'
 import Authentication from './routes/authentication/authentication.component'
 import Shop from './routes/shop/shop.component'
+import Checkout from './routes/checkout/checkout.component'
 
 const App = () => {
   return (
@@ -22,6 +23,10 @@ const App = () => {
         <Route
           path="shop"
           element={<Shop />}
+        />
+        <Route
+          path="checkout"
+          element={<Checkout />}
         />
       </Route>
     </Routes>
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react'
+import { CartContext } from '../../context/cart.context'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+
+import './checkout.styles.scss'
+
+const Checkout = () => {
+  const { cartItems, cartTotal } = useContext(CartContext)
+  return (
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>PRODUCT</span>
+        </div>
+        <div className="header-block">
+          <span>DESCRIPTION</span>
+        </div>
+        <div className="header-block">
+          <span>QUANTITY</span>
+        </div>
+        <div className="header-block">
+          <span>PRICE</span>
+        </div>
+        <div className="header-block">
+          <span>REMOVE</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem
+          key={cartItem.id}
+          cartItem={cartItem}
+        />
+      ))}
+      <span className="total">Total: ${cartTotal}</span>
+    </div>
+  )
+}
+export default Checkout
